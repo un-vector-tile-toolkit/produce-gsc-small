fa3c62e60c75b20948fec57a579db289a4bb4929
ubukawa <ubukawa-t96dq@mlit.go.jp>
2020-07-10 17:22:31 -0400

    newly developed
    created a repository

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # produce-gsc-small
 small scale tile production
+
+
+# How to use
+vi config/default.hjson  
+mkdir /data/small-zxy  
+
+npm install  
+node index.js
+
+If you are running this at windows environment, please use docker (for tippecanoe).  
+
+Need to have gdal greater than 2.4.0  
+If not,     
+sudo apt update  
+sudo apt install software-properties-common  
+sudo apt-get install python3.6-dev  
+sudo add-apt-repository ppa:ubuntugis/ppa  
+sudo apt-get install gdal-bin  
---
+++ index.js
@@ -0,0 +1,66 @@
+const config = require('config')
+const Parser = require('json-text-sequence').parser
+const { spawn } = require('child_process')
+
+const minzoom = config.get('minzoom')
+const maxzoom = config.get('maxzoom')
+const projection = config.get('projection')
+const srcs = config.get('srcs')
+const ogr2ogrPath = config.get('ogr2ogrPath')
+const tippecanoePath = config.get('tippecanoePath')
+const dstDir = config.get('dstDir')
+const host = config.get('host')
+const port = config.get('port')
+const dbname = config.get('dbname')
+const user = config.get('user')
+const password = config.get('password')
+
+const tippecanoe = spawn(tippecanoePath, [
+  `--output-to-directory=${dstDir}`,
+  `--no-tile-compression`,
+  `--drop-rate=1`,
+  '--force',
+  `--minimum-zoom=${minzoom}`,
+  `--maximum-zoom=${maxzoom}`,
+  `--projection=EPSG:${projection}`
+], { stdio: ['pipe', 'inherit', 'inherit'] })
+const downstream = tippecanoe.stdin
+
+const preprocessProperties = (f) => {
+ if (f.properties['element']) {
+  delete f.properties['element']
+    }
+  return f
+}
+
+let nOpenFiles = 0
+for (const src of srcs) {
+  nOpenFiles++
+  const parser = new Parser()
+    .on('data', f => {
+      f = preprocessProperties(f)
+      f.tippecanoe = {
+        layer: src.layer,
+        projection: src.projection,
+        minzoom: src.minzoom,
+        maxzoom: src.maxzoom,
+        projection: src.projection
+      }
+      downstream.write(`\x1e${JSON.stringify(f)}\n`)
+    })
+    .on('finish', () => {
+      nOpenFiles--
+      if (nOpenFiles === 0) {
+        downstream.end()
+      }
+    })
+  const ogr2ogr = spawn(ogr2ogrPath, [
+    '-f', 'GeoJSONSeq',
+    '-lco', 'RS=YES',
+    '/vsistdout/',
+    `PG:host=${host} port=${port} dbname=${dbname} user=${user} password=${password}`,
+    '-sql',    
+    src.sql
+  ])
+  ogr2ogr.stdout.pipe(parser)
+}
